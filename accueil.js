289c2a7be13c5768e3ee05067663eb07a261ce75
Saphyr25 <riyad2593@gmail.com>
2023-06-02 16:58:25 +0200

    nav banniere text defilant

--- accueil.js
+++ accueil.js
@@ -4,3 +4,21 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+
+  let index = 0;
+  displayImages();
+
+  function displayImages() {
+    let i;
+    const images = document.getElementsByClassName("image");
+    for (i = 0; i < images.length; i++) {
+      images[i].style.display = "none";
+    }
+    index++;
+    if (index > images.length) {
+      index = 1;
+    }
+    images[index-1].style.display = "block";
+    setTimeout(displayImages, 2000); 
+  }
