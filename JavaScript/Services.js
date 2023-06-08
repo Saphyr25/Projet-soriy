133329f7bef8e67fc6f50b2e8b60e21e9809d24a
Saphyr25 <riyad2593@gmail.com>
2023-06-08 16:56:48 +0200

    fin page contact page service 50%

---
+++ JavaScript/Services.js
@@ -0,0 +1,29 @@
+function openServices(ServicesName) {
+    var i;
+    var x = document.getElementsByClassName("Services");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(ServicesName).style.display = "block";  
+  }
+
+
+  
+  
+var div1 = document.getElementById("div1");
+var div2 = document.getElementById("div2");
+
+div1.addEventListener("click", function() {
+  div2.style.background="#A76F51";
+  div1.style.background=" #C7815C";
+
+});
+
+div2.addEventListener("click", function() {
+  div1.style.background="#A76F51";
+  div2.style.background=" #C7815C";
+});
+
+
+
+
