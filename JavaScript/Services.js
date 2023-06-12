59424d029a2694cd0eba23195846436e93ce8171
Riyad <riyad2593@gmail.com>
2023-06-12 16:13:24 +0200

    test

--- JavaScript/Services.js
+++ JavaScript/Services.js
@@ -1,24 +1,61 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// Fin de la nav
+let index = 0;
+displayImages();
+
+function displayImages() {
+  let a;
+  const images = document.getElementsByClassName("image");
+  for (a = 0; a < images.length; a++) {
+    images[a].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2500); 
+}
+
+
+
+
+
+
+// Création de la fonction openServices qui va s'activer lors d'un onclick et qui va nous permettre de spécifier des éléments
 function openServices(ServicesName) {
+  // initialisation de la var i sans valeur
     var i;
+    // initialisation de la var x avec comme valeur tous les éléments ayan comme class "services"
     var x = document.getElementsByClassName("Services");
+    // Boucle for qui va permettre de definir un display none a tous les éléments services
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
+    // initialisation d'un display flex a l'élément cliqué qui va le rendre visible
     document.getElementById(ServicesName).style.display = "flex";  
   }
 
 
   
-  
+  // initialisation de la var div1 qui a pour valeur l'ID div1 
 var div1 = document.getElementById("div1");
+  // initialisation de la var div2 qui a pour valeur l'ID div12
 var div2 = document.getElementById("div2");
-
+// cette ligne de code crée un event , lorsqu'on clique sur div1 une fonctions éxecute
 div1.addEventListener("click", function() {
+  // la couleurs  des div sont alternés
   div2.style.background="#A76F51";
   div1.style.background=" #C7815C";
 
 });
-
+// cette ligne de code crée un event , lorsqu'on clique sur div2 une fonctions éxecute
 div2.addEventListener("click", function() {
   div1.style.background="#A76F51";
   div2.style.background=" #C7815C";
--- JavaScript/accueil.js
+++ JavaScript/accueil.js
@@ -1,24 +1,37 @@
+// ouverture et fermeture de l'overlay lors du clique
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+  // relié au onclick du menu hamburger, l overlay s'ouvrira lors d'un clique
+    document.getElementById("myNav").style.width = "100%"; 
   }
   function closeNav() {
+    // relié au bouton en croix dans l'overlay , l'overlay se fermera lors d'un clique
     document.getElementById("myNav").style.width = "0%";
   }
 
-
+// Slide Show
+// Création de la variable index avec une valeur donné de 0
   let index = 0;
-  displayImages();
 
+  // Création de la fonction displayImages
+  displayImages();
   function displayImages() {
+  // initialisation de la variable i sans valeur pour l'instant
     let i;
-    const images = document.getElementsByClassName("image");
+  // initialiation de la var images qui regroupe tous les éléments possédant la class "image"
+    var images = document.getElementsByClassName("image");
+   //Boucle for qui nous dis que chaque éléments image est parcourus et voit son display définie sur none  
     for (i = 0; i < images.length; i++) {
       images[i].style.display = "none";
     }
+    // ici on incrémente index ce qui nous déplace vers la prochaine image
     index++;
+    //on verifie la condition: si index est supérieur a la longueur d'images (le nombre d'images)donc si toutes les images ont été parcourus
     if (index > images.length) {
+    //si cest le cas: on réinitialise index a 1 pour revenir a la premiere image
       index = 1;
     }
+    // l'image correspondant a l'index actuelle se voit définir un display block( elle devient donc visible)
     images[index-1].style.display = "block";
+    // la fonction displayImages sera rappelé apres un delai de 2500 millisecondes
     setTimeout(displayImages, 2500); 
   }
